b6e54b571c838c4bcdc2d9f1161b0d2be2fdc9f4
yamen sahyouni <yamen.sahyouni@gmail.com>
2024-05-03 19:43:39 +0200

    add spring boot application

---
+++ backendbms/src/main/java/com/bookmanagsys/backendbms/BackendbmsApplication.java
@@ -0,0 +1,13 @@
+package com.bookmanagsys.backendbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendbmsApplication.class, args);
+	}
+
+}
---
+++ backendbms/src/test/java/com/bookmanagsys/backendbms/BackendbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmanagsys.backendbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
