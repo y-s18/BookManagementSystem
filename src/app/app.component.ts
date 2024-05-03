3ada5c613b9eba4caae8e3a6923248fc26f7061b
yamen sahyouni <yamen.sahyouni@gmail.com>
2024-05-03 19:24:38 +0200

    configure routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BookService } from './services/book.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [BookService]
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Book Management System';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { UpdateBookComponent } from './components/update-book/update-book.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'books', component: BookListComponent},
+  {path: 'add-book', component: AddBookComponent},
+  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: 'update-book', component: UpdateBookComponent},
+  {path: 'book-details', component: BookDetailsComponent}
+];
