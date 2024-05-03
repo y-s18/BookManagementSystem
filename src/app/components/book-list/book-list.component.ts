4563bb85352d14a7bb046f001fb449215ce312bd
yamen sahyouni <yamen.sahyouni@gmail.com>
2024-05-03 15:23:49 +0200

    implement updating, removing and retrieving book list

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../../model/book';
+import { BookService } from '../../services/book.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.css'
+  styleUrl: './book-list.component.css',
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  books!: Book[];
 
+  constructor(private bookService: BookService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks() {
+    this.bookService.getBooksList().subscribe((data) => {
+      this.books = data;
+    });
+  }
+
+  bookDetails(id: number) {
+    this.router.navigate(['book-details', id]);
+  }
+
+  updateBook(id: number) {
+    this.router.navigate(['update-book', id]);
+  }
+
+  deleteBook(id: number) {
+    this.bookService.deleteBook(id).subscribe((data) => {
+      console.log(data);
+      this.getBooks();
+    });
+  }
 }
