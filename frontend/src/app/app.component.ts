36607ed868cb94c1fd79325fc21dcee0918a748f
yamen sahyouni <yamen.sahyouni@gmail.com>
2024-05-04 11:44:48 +0200

    fix issue where routerlink not navigating to corresponding component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { BookService } from './services/book.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [BookService]
